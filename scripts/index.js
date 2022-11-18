75b9dbc12a6308f8c7934058d4efce7b416fb32e
Marsel Sagitov <outbin@yandex.ru>
2022-11-18 16:11:15 +0300

    feature/default cards js

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,99 @@
 let editElement = document.querySelector('.profile__edit');
+let closeElement = document.querySelector('.popup__close');
 let popupElement = document.querySelector('.popup');
-let popupCloseElement = popupElement.querySelector('.popup__close');
-let titleElement = document.querySelector('.profile__title');
-let subtitleElement = document.querySelector('.profile__subtitle');
-let nameInput = document.querySelector('.popup__input_profile_name');
-let nameJob = document.querySelector('.popup__input_profile_job');
+let nameProfileElement = document.querySelector('.profile__title');
+let jobProfileElement = document.querySelector('.profile__subtitle');
+let inputNameElement = document.querySelector('.popup__input_profile_name');
+let inputJobElement = document.querySelector('.popup__input_profile_job');
 let formElement = document.querySelector('.popup__form');
 
+// opening a popup and adding titles to an input from a profile when you click on the edit button
+
 function popupOpen() {
-  nameInput.value = titleElement.textContent;
-  nameJob.value = subtitleElement.textContent;
+  inputNameElement.value = nameProfileElement.textContent;
+  inputJobElement.value = jobProfileElement.textContent;
   popupElement.classList.add('popup_opened');
 }
+
+editElement.addEventListener('click', popupOpen);
+
+// popup closing when you click on the close button
 
 function popupClose() {
   popupElement.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+closeElement.addEventListener('click', popupClose);
+
+// editing and saving profile titles
+
+function formSubmitHandler (evt) {
   evt.preventDefault();
-  titleElement.textContent = nameInput.value;
-  subtitleElement.textContent = nameJob.value;
+  nameProfileElement.textContent = inputNameElement.value;
+  jobProfileElement.textContent = inputJobElement.value;
   popupClose();
 }
-editElement.addEventListener('click', popupOpen);
-popupCloseElement.addEventListener('click', popupClose);
+
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+//six default cards
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// get template
+
+const cardTemplate = document.querySelector('#template').content.querySelector('.elements__item');
+
+// cards generating
+
+const generateCard = (dataCard) => {
+  const newCard = cardTemplate.cloneNode(true);
+
+  const name = newCard.querySelector('.elements__title');
+  name.textContent = dataCard.name;
+
+  const link = newCard.querySelector('.elements__img');
+  link.src = dataCard.link;
+
+  return newCard;
+};
+
+// cards adding
+
+const cardContainer = document.querySelector('.elements__list');
+
+const renderCard = (dataCard) => {
+  cardContainer.prepend(generateCard(dataCard));
+};
+
+// cards rendering
+
+initialCards.forEach((dataCard) => {
+  renderCard(dataCard);
+})
